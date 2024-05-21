2b2d5fad7643ae635af0da0e04cf7fc20c9d63f5
Oscar Menjívar <pinochofino@gmail.com>
2024-05-21 10:41:09 -0600

    Change oauth

--- oauth-microservice/src/main/java/com/shoppingcart/oauth/services/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingcart.oauth.services;
-
-import com.shoppingcart.commons.users.models.entity.User;
-
-public interface IUserService {
-    public User findByUsername(String username);
-
-}
