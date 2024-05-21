453723b4a371a71fab2eb5ecb1c5b789521a9cf1
Oscar Menjívar <pinochofino@gmail.com>
2024-05-21 10:41:38 -0600

    Ouath microservice updated

---
+++ oauth-microservice/src/test/java/com/shoppingcart/oauth/OauthMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
