a73f110cec2312209b81b8561cfc66c5f611c28e
Oscar Menjívar <pinochofino@gmail.com>
2024-05-21 10:39:38 -0600

    Dismiss products

--- products-microservice/src/main/java/com/shoppingcart/productos/models/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingcart.productos.models.dao;
-
-import com.shoppingcart.productos.models.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductDao extends CrudRepository<Product,Long> {
-
-}
--- products-microservice/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into products (name, price, create_at) values ('Panasonic',800,NOW());
-insert into products (name, price, create_at) values ('Sony',900,NOW());
-insert into products (name, price, create_at) values ('Toshiba',100,NOW());
-insert into products (name, price, create_at) values ('Mac',200,NOW());
-insert into products (name, price, create_at) values ('Iphone',300,NOW());
